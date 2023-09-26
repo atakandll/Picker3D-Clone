506b3f866314eb1b17ac145e5d328808551498e3
atakandll <atakanccnar@outlook.com>
2023-09-26 20:47:24 +0300

    Input system final init

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using Data.UnityObjects;
 using Data.ValueObjects;
+using Keys;
 using Signals;
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace Managers
 {
@@ -17,7 +18,7 @@ namespace Managers
         private bool _isAvailableForTouch, _isFirstTimeTouchTaken, _isTouching;
 
         private float _currentVelocity;
-        private float _moveVector;
+        private Vector2 _moveVector;
         private Vector2? _mousePosition;
 
         #endregion
@@ -81,10 +82,76 @@ namespace Managers
         {
             if (!_isAvailableForTouch) return;
 
-            if (Input.GetMouseButtonUp(0) && !IsPointerOverUIElement()) // UI kısmına değmiyorsam
+            if (Input.GetMouseButtonUp(0) && !IsPointerOverUIElement()) // UI kısmına değmiyorsam, elimizi çektiğimiz kısım
             {
                 _isTouching = false;
+                InputSignals.Instance.onInputReleased?.Invoke(); 
+                Debug.LogWarning("Executed -----> OnIputReleased");
+                
+                
             }
+
+            if (Input.GetMouseButtonDown(0) && !IsPointerOverUIElement()) // ilk tıkladığım an
+            {
+                _isTouching = true;
+                InputSignals.Instance.onInputTaken?.Invoke();
+                Debug.LogWarning("Executed -----> OnIputTaken");
+                
+                if (!_isFirstTimeTouchTaken)
+                {
+                    _isFirstTimeTouchTaken = true;
+                    InputSignals.Instance.onFirstTimeTouchTaken?.Invoke();
+                    Debug.LogWarning("Executed -----> OnFirstTimeTouchTaken");
+                    
+                }
+
+                _mousePosition = Input.mousePosition;
+            }
+
+            if (Input.GetMouseButton(0) && !IsPointerOverUIElement())
+            {
+                if (_isTouching)
+                {
+                    if (_mousePosition != null)
+                    {
+                        Vector2 mouseDeltaPos = (Vector2)Input.mousePosition - _mousePosition.Value;
+                        
+                        if (mouseDeltaPos.x > _data.HorizontalInputSpeed)
+                        {
+                            _moveVector.x = _data.HorizontalInputSpeed / 10f * mouseDeltaPos.x;
+                        }
+                        else if (mouseDeltaPos.x < _data.HorizontalInputSpeed)
+                        {
+                            _moveVector.x = -_data.HorizontalInputSpeed / 10f * mouseDeltaPos.x;
+                        }
+                        else // hissiyat için yaptık, yavaş yavaş 0 olucak.
+                        {
+                            _moveVector.x = Mathf.SmoothDamp(_moveVector.x, 0, ref _currentVelocity, _data.ClampSpeed);
+                        }
+
+                        _mousePosition = Input.mousePosition;
+                        
+                        InputSignals.Instance.onInputDragged?.Invoke(new HorizantalInputParams()
+                        {
+                            HorizantalValue = _moveVector.x,
+                            ClampValues =   _data.ClampValues
+                            
+                        });
+                        
+                    }
+                }
+            }
+        }
+
+        private bool IsPointerOverUIElement()
+        {
+            var eventData = new PointerEventData(EventSystem.current)
+            {
+                position = Input.mousePosition
+            };
+            var results = new List<RaycastResult>();
+            EventSystem.current.RaycastAll(eventData, results);
+            return results.Count > 0;
         }
     }
 }
