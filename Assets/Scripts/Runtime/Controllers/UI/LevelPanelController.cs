8953626e36e1d72bfc72626e00dcf6d96094eed7
atakandll <atakanccnar@outlook.com>
2023-09-29 20:16:47 +0300

    Level Panel Controller (added DOTWEEN pro)

---
+++ Assets/Scripts/Runtime/Controllers/UI/LevelPanelController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using DG.Tweening;
+using Runtime.Signals;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Runtime.Controllers.UI
+{
+    public class LevelPanelController : MonoBehaviour
+    {
+        #region Self Variables
+
+        #region  Serialized Variables
+
+        [SerializeField] private List<Image> stageImages = new List<Image>();
+        [SerializeField] private List<TextMeshProUGUI> levelTexts = new List<TextMeshProUGUI>();
+
+        #endregion
+
+        #endregion
+
+        private void OnEnable()
+        {
+            SubscribeEvent();
+        }
+
+        private void SubscribeEvent()
+        {
+            UISignals.Instance.onSetStageColor += OnSetStageColor;
+            UISignals.Instance.onSetLevelValue += OnSetLevelValue;
+        }
+
+        private void OnSetLevelValue(byte levelValue)
+        {
+            var additionalValue = ++levelValue; // 1 yazması için
+            
+            levelTexts[0].text = additionalValue.ToString();
+            
+            additionalValue++;
+            
+            levelTexts[1].text = additionalValue.ToString();
+        }
+
+        private void OnSetStageColor(byte stageValue)
+        {
+            stageImages[stageValue].DOColor(new Color(0.9960785f,0.4196079f,0.07843138f), 0.5f);
+        }
+
+        private void OnDisable()
+        {
+            UnSubscribeEvent();
+        }
+
+        private void UnSubscribeEvent()
+        {
+            UISignals.Instance.onSetStageColor -= OnSetStageColor;
+            UISignals.Instance.onSetLevelValue -= OnSetLevelValue;
+        }
+    }
+}
