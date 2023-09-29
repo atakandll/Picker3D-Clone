070234808d9bcc0c606d7a37686448963c3ccd18
atakandll <atakanccnar@outlook.com>
2023-09-29 20:52:58 +0300

    Camera System

---
+++ Assets/Scripts/Runtime/Managers/CameraManager.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Cinemachine;
+using Runtime.Signals;
+using Unity.Mathematics;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Runtime.Managers
+{
+    public class CameraManager : MonoBehaviour
+    {
+        #region Self variables
+
+        #region Serialized Variables
+
+        [SerializeField] private CinemachineVirtualCamera virtualCamera;
+
+        #endregion
+
+        #region Private Variables
+
+        private float3 _firstPosition;
+
+        #endregion
+
+        #endregion
+
+        private void Start()
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+            _firstPosition = transform.position;
+        }
+
+        private void OnEnable()
+        {
+            SubscribeEvent();
+        }
+
+        private void SubscribeEvent()
+        {
+            CameraSignals.Instance.onSetCameraTarget += OnSetCameraTarget;
+            CoreGameSignals.Instance.onReset += OnReset;
+        }
+
+        private void OnReset()
+        {
+            transform.position = _firstPosition;
+        }
+
+        private void OnSetCameraTarget()
+        {
+           // var player = FindObjectOfType<PlayerManager>().transform;
+           // virtualCamera.Follow = player;
+           // virtualCamera.LookAt = player;
+        }
+
+
+        private void UnSubscribeEvent()
+        {
+            CameraSignals.Instance.onSetCameraTarget -= OnSetCameraTarget;
+            CoreGameSignals.Instance.onReset -= OnReset;
+        }
+        private void OnDisable()
+        {
+            UnSubscribeEvent();
+        }
+    }
+}
---
+++ Assets/Scripts/Runtime/Signals/CameraSignals.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Runtime.Signals
+{
+    public class CameraSignals : MonoBehaviour
+    {
+        #region Singleton
+
+        public static CameraSignals Instance;
+        
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            Instance = this;
+        }
+
+        #endregion
+        
+        public UnityAction onSetCameraTarget = delegate {  };
+    }
+}
